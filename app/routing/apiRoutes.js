f1085c4f66b32435db92373552a6e804c421d939
Byron Anderson <Byron Anderson>
2017-05-15 18:43:16 -0400

    completed

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,14 +8,28 @@ module.exports = function(app) {
         res.json(friendsData);
     });
 
-    app.post('/api/new', function(req, res) {
+    app.post('/api/friends', function(req, res) {
         var newFriend = req.body;
-
-        console.log(newFriend);
+        console.log(newFriend.scores);
+        var index = compare(friendsData, newFriend)       
         friendsData.push(newFriend);
 
-        function compare() {
-        
+    function compare(friends, self) {
+        var differenceArray = [];
+        for (var i = 0; i < friends.length; i++){
+            var totalDifference = 0;
+            for (var j = 0; j < 10; j++){
+               var difference = Math.abs(friends[i].scores[j] - self.scores[j]);
+               totalDifference += difference;
+            }
+            differenceArray.push(totalDifference);       
         }
+        var closestNerd = Math.min.apply(null, differenceArray);
+        var index = differenceArray.indexOf(closestNerd);
+        return index;
+    }
+    console.log(friendsData[index]);
+        res.json(friendsData[index]);
     });
 }
+
